4631ef45719840372452abd7e14c18d0403dbc32
unknown <saraharyandi@gmail.com>
2025-12-23 10:59:53 +0700

    fix docker local folder

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,19 +6,18 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from joblib import dump
 import os
+import shutil
 import dagshub
-dagshub.init(
-    repo_owner='pemujacoding',
-    repo_name='medical_cost_model',
-    mlflow=True,
+
+def modelling():
+    # DagsHub/MLflow setup
+    dagshub.init(
+        repo_owner='pemujacoding',
+        repo_name='medical_cost_model',
+        mlflow=True,
     )
-
-def modelling() :
-
-    # MLflow dagshub
+    
     mlflow.set_tracking_uri("https://dagshub.com/pemujacoding/medical_cost_model.mlflow/")
-
-    # MLflow setup
     experiment_name = "Online Training Medical Cost"
 
     existing_experiment = mlflow.get_experiment_by_name(experiment_name)
@@ -28,10 +27,8 @@ def modelling() :
 
     # Load preprocessed data
     df = pd.read_csv("MLProject/medical_cost_preprocessed.csv")
-
     X = df.drop(columns=["annual_medical_cost"])
     y = df["annual_medical_cost"]
-
 
     model = SGDRegressor(
         max_iter=1, 
@@ -46,25 +43,36 @@ def modelling() :
         for i in range(0, len(X), batch_size):
             X_batch = X.iloc[i:i + batch_size]
             y_batch = y.iloc[i:i + batch_size]
-
-            # Incremental training
             model.partial_fit(X_batch, y_batch)
 
-        # Evaluation (after training)
+        # Evaluation
         preds = model.predict(X)
-        mse = mean_squared_error(y, preds)
-        rmse = np.sqrt(mse)
+        rmse = np.sqrt(mean_squared_error(y, preds))
         r2 = r2_score(y, preds)
 
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2_score", r2)
 
+        # 1. Save as a single file (for your artifact upload step)
         dump(model, "MLProject/online_sgd_model.joblib")
         mlflow.log_artifact("MLProject/online_sgd_model.joblib")
 
+        # 2. Log to DagsHub (Remote)
         mlflow.sklearn.log_model(
             sk_model=model,
             artifact_path="online_model",
+            input_example=X.iloc[:5]
+        )
+
+        # 3. SAVE LOCALLY (For Docker build step)
+        # This creates the MLProject/online_model folder with MLmodel metadata
+        local_model_path = "MLProject/MLProject"
+        if os.path.exists(local_model_path):
+            shutil.rmtree(local_model_path)
+            
+        mlflow.sklearn.save_model(
+            sk_model=model,
+            path=local_model_path,
             input_example=X.iloc[:5]
         )
 
